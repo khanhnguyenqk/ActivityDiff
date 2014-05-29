f0352ef778ef8e4faacbd6190d2c571574b1a1cb
Khanh Nguyen <khanhnguyen.qk@gmail.com>
2014-05-28 20:37:17 -0700

    Initiation
    Create projects and some work on data type

---
+++ XmlItemDiffTool/XmlItemDiffTool/Infrastructure/DataType/XmlItem.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace Infrastructure.DataType
+{
+    public class XmlItem : NotifyPropertyChangedBase
+    {
+        private string id;
+        public string Id
+        {
+            get { return id; }
+            set
+            {
+                if(!value.Equals(id))
+                {
+                    id = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+
+    }
+}
---
+++ XmlItemDiffTool/XmlItemDiffTool/Infrastructure/DataType/XmlItemProperty.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Infrastructure.DataType
+{
+    public class XmlItemProperty : NotifyPropertyChangedBase
+    {
+        private string name = String.Empty;
+        /// <summary>
+        /// Not Nullable
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if(value != null && !value.Equals(name))
+                {
+                    value = name;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        private string value = String.Empty;
+        public string Value
+        {
+            get { return value; }
+            set
+            {
+                if(value != null && !value.Equals(value))
+                {
+                    value = name;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            int ret = Name.GetHashCode();
+            ret = ret << 20;
+            ret = ret ^ Value.GetHashCode();
+            return ret;
+        }
+
+        public override bool Equals(Object obj)
+        {
+            // If parameter is null return false.
+            if(obj == null)
+            {
+                return false;
+            }
+
+            // If parameter cannot be cast to Point return false.
+            XmlItemProperty x = obj as XmlItemProperty;
+            if((System.Object)x == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return Value.Equals(x.Value) && Name.Equals(x.Name);
+        }
+
+        public bool Equals(XmlItemProperty x)
+        {
+            // If parameter is null return false:
+            if((object)x == null)
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return Value.Equals(x.Value) && Name.Equals(x.Name);
+        }
+    }
+}
