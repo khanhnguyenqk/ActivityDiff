53ff9f9889864956fd7ffa4b73902eb718799e68
Khanh Nguyen <khanhnguyen.qk@gmail.com>
2014-05-31 01:14:27 -0700

    New enum

---
+++ XmlItemDiffTool/XmlItemDiffTool/Infrastructure/Enum/HistoryState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Enum
+{
+    public enum HistoryState
+    {
+        N, // No Changed
+        A, // Added
+        R, // Removed
+        M, // Moved to the same level of the tree
+        L, // Moved to different level of the tree,
+        C  // Changed (Inner property change OR children changed)
+    }
+}
